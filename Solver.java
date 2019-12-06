5d0d5820ef661a50d92b7a2ff5d78f5633162345
0x44494D41 <dmtrmlnk@gmail.com>
2019-12-05 19:25:26 -0500

    Stepper Adjustment
    Now, you don't get the 'first step' right off the bat. Neato!

--- Solver.java
+++ Solver.java
@@ -13,12 +13,12 @@ public class Solver {
 		Scanner stepper = new Scanner(System.in);
 		
 		if(n == 1) {
-			allColumns[position].getLast().moveRing(allColumns, position, target);
 			stepper.nextLine();
+			allColumns[position].getLast().moveRing(allColumns, position, target);
 		} else {
 			moveStack(n-1, position, target, swap, allColumns);
-			allColumns[position].getLast().moveRing(allColumns, position, target); //NOTE: this n-1 refers to the array listing referencing the CURRENT Ring, not the one above it
 			stepper.nextLine();
+			allColumns[position].getLast().moveRing(allColumns, position, target); //NOTE: this n-1 refers to the array listing referencing the CURRENT Ring, not the one above it
 			moveStack(n-1, swap, position, target, allColumns);
 		}
 		System.out.println("out of "+n);
